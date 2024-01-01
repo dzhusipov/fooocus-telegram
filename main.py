2ef37ece868eb03e72bc7026092716fd6657defa
DAsm <zh.daur@gmail.com>
2024-01-01 20:47:31 +0600

    async

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import shutil
 import logging
-
-from telegram import Update
+import time
+import base64
+from telegram import Update, InputMediaPhoto
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import requests
 import os
 
+fooocus_url = "http://100.71.129.59:8888/v1/generation/text-to-image"
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,9 +19,95 @@ logging.basicConfig(
 )
 
 
+async def get_job_status(job_id):
+    url = 'http://100.71.129.59:8888/v1/generation/query-job'
+    params = {
+        'job_id': job_id,
+        'require_step_preivew': True
+    }
+
+    response = requests.get(url, params=params)
+    logging.info(response.json())
+    return response.json()
+
+
+async def call_fooocus_async(prompt, performance):
+    logging.info("Calling fooocus async with prompt: " + prompt)
+
+    # url = "http://192.168.1.42:8888/v1/generation/text-to-image"
+    # performance = "Quality"
+    # performance = "Speed"
+    # performance = "Balanced"
+    data = {
+              "prompt": prompt,
+              "negative_prompt": "",
+              "style_selections": [
+                "Fooocus V2",
+                "Fooocus Enhance",
+                "Fooocus Sharp"
+              ],
+              "performance_selection": performance,
+              "aspect_ratios_selection": "1152*896",
+              "image_number": 1,
+              "image_seed": -1,
+              "sharpness": 2,
+              "guidance_scale": 4,
+              "base_model_name": "juggernautXL_version6Rundiffusion.safetensors",
+              "refiner_model_name": "None",
+              "refiner_switch": 0.5,
+              "loras": [
+                {
+                  "model_name": "sd_xl_offset_example-lora_1.0.safetensors",
+                  "weight": 0.1
+                }
+              ],
+              "advanced_params": {
+                "adaptive_cfg": 7,
+                "adm_scaler_end": 0.3,
+                "adm_scaler_negative": 0.8,
+                "adm_scaler_positive": 1.5,
+                "canny_high_threshold": 128,
+                "canny_low_threshold": 64,
+                "controlnet_softness": 0.25,
+                "debugging_cn_preprocessor": False,
+                "debugging_inpaint_preprocessor": False,
+                "disable_preview": False,
+                "freeu_b1": 1.01,
+                "freeu_b2": 1.02,
+                "freeu_enabled": False,
+                "freeu_s1": 0.99,
+                "freeu_s2": 0.95,
+                "inpaint_disable_initial_latent": False,
+                "inpaint_engine": "v1",
+                "inpaint_respective_field": 1,
+                "inpaint_strength": 1,
+                "mixing_image_prompt_and_inpaint": False,
+                "mixing_image_prompt_and_vary_upscale": False,
+                "overwrite_height": -1,
+                "overwrite_step": -1,
+                "overwrite_switch": -1,
+                "overwrite_upscale_strength": -1,
+                "overwrite_vary_strength": -1,
+                "overwrite_width": -1,
+                "refiner_swap_method": "joint",
+                "sampler_name": "dpmpp_2m_sde_gpu",
+                "scheduler_name": "karras",
+                "skipping_cn_preprocessor": False
+              },
+              "require_base64": False,
+              "async_process": True,
+              "webhook_url": "string"
+            }
+    response = requests.post(fooocus_url, json=data)
+    logging.info(response.json())
+
+    job_id = response.json()['job_id']
+    # download image by url to tmp folder
+    return job_id
+
+
 async def call_fooocus(prompt, performance):
     logging.info("Calling fooocus with prompt: " + prompt)
-    url = "http://100.71.129.59:8888/v1/generation/text-to-image"
     # url = "http://192.168.1.42:8888/v1/generation/text-to-image"
     # performance = "Quality"
     # performance = "Speed"
@@ -84,7 +172,7 @@ async def call_fooocus(prompt, performance):
               "async_process": False,
               "webhook_url": "string"
             }
-    response = requests.post(url, json=data)
+    response = requests.post(fooocus_url, json=data)
     logging.info(response.json())
 
     image_url = response.json()[0]['url'].replace("127.0.0.1", "100.71.129.59")
@@ -129,16 +217,59 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def make(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Removing "/make" from the string
     result = update.message.text.replace("/make", "").strip()
-    await update.message.reply_text(f'Starting generate image with prompt: {result}')
+    identifier = await update.message.reply_text(f'Starting generate...')
+
+    await identifier.edit_text(f'Generating image with prompt: {result}')
     done_url = await call_fooocus(result, "Speed")
     file = await get_image_url(done_url)
     await update.message.reply_photo(file)
 
 
+async def make_async(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Removing "/make" from the string
+    result = update.message.text.replace("/async", "").strip()
+    text_identifier = await update.message.reply_text(f'Starting generate...')
+
+    job_id = await call_fooocus_async(result, "Speed")
+    job_status = await get_job_status(job_id)
+    image_identifier = None
+    job_progress = ""
+    while job_status["job_stage"] == "RUNNING" or job_status["job_stage"] == "WAITING":
+        old_job_progress = job_progress
+        if job_status["job_stage"] == "RUNNING" and job_status["job_step_preview"] is not None:
+            if image_identifier is None:
+                image_identifier = await update.message.reply_photo(base64.b64decode(job_status["job_step_preview"]))
+            else:
+                # remove old image
+                new_media = InputMediaPhoto(media=base64.b64decode(job_status["job_step_preview"]))
+                await image_identifier.edit_media(media=new_media)
+                # await image_identifier.delete()
+                # image_identifier = await update.message.reply_photo(base64.b64decode(job_status["job_step_preview"]))
+        try:
+            await text_identifier.edit_text(f'Job progress: {job_progress}%')
+        except:
+            pass
+
+        time.sleep(1)
+        job_status = await get_job_status(job_id)
+        job_progress = job_status["job_progress"]
+
+    await image_identifier.delete()
+    await text_identifier.edit_text(f'Job progress: 100%')
+    result_image = job_status["job_result"][0]["url"].replace("127.0.0.1", "100.71.129.59")
+
+    file = await get_image_url(result_image)
+    await update.message.reply_photo(file)
+
+
+def progress_bar(percentage):
+    max_length = 50  # Define the length of the progress bar
+    filled_length = int(max_length * percentage // 100)  # Calculate filled length
+    bar = '█' * filled_length + '-' * (max_length - filled_length)  # Create the bar
+    return f"[{bar}] {percentage}% Complete"  # Fo
+
+
 app = ApplicationBuilder().token("6778017668:AAEO_z45JQkygK1T8_J8e98GeBTSnOqFaAI").build()
-
-
-# app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("make", make))
-
+app.add_handler(CommandHandler("async", make_async))
 app.run_polling()
