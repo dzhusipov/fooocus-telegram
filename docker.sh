cafa4b1dcab638ef5789a6b7577d04fdc974a67a
Жусипов Даурхан Асханович <daurkhan.zhussipov@kaspi.kz>
2024-07-05 09:24:05 +0500

    git pull before docker run

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 docker ps | grep fooocus-telegram | awk '{print $1}' | xargs docker stop || true
 docker ps -a | grep fooocus-telegram | awk '{print $1}' | xargs docker rm || true
 docker rmi fooocus-telegram || true
