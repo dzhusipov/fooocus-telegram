f3fb8d8be590aed9ac0110a36a7c71af82f5017c
Жусипов Даурхан Асханович <daurkhan.zhussipov@kaspi.kz>
2024-07-05 09:19:29 +0500

    open ai images and competions

--- bot_commands.py
+++ bot_commands.py
@@ -4,6 +4,8 @@ import time
 import os
 import base64
 import subprocess
+import requests
+
 from dotenv import load_dotenv
 from telegram import Update, InputMediaPhoto
 from telegram.ext import CommandHandler, ContextTypes, MessageHandler, filters
@@ -15,6 +17,98 @@ load_dotenv()
 FOOOCUS_IP = os.getenv("FOOOCUS_IP")
 ADMIN_ID = os.getenv("ADMIN_ID")
 GROUP_ID = os.getenv("GROUP_ID")
+OPENAPI_KEY = os.getenv("OPENAPI_KEY")
+
+async def image_gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    
+    print(".........image_gpt.........")
+    print(update)
+    
+    if update.message.chat.id not in [int(GROUP_ID), int(ADMIN_ID), -1002122545639, -1001772550506]:
+        await update.message.reply_text("Sorry, you can't use this bot")
+        return
+    
+    await update.message.reply_text("Uno momento por favor...")
+    result = update.message.text.replace("/igpt", "").strip()
+    
+    url = "https://api.openai.com/v1/images/generations"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAPI_KEY}"
+    }
+    data = {
+        "model": "dall-e-3",
+        "prompt": result,
+        "n": 1,
+        "size": "1024x1024"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    print(response.json())
+
+    # Print the image URL
+    image_url = response.json()['data'][0]['url']
+    
+    file_path = await get_image_url(image_url)
+    await update.message.reply_photo(file_path)
+    
+    # image to base64 string
+    with open(file_path, 'rb') as file:  # 'rb' for reading in binary mode
+        image_data = base64.b64encode(file.read())
+    
+    try:
+        logging.info("starting DB insert")
+        add_user_history_record_pg(update.effective_user.id, result, image_data)
+    except Exception as e:
+        logging.error("Unhandled database exception: %s", e)
+
+
+def extract_text(input_string):
+    # Find the first and last occurrences of the double quote
+    first_quote = input_string.find('"')
+    last_quote = input_string.rfind('"')
+    
+    # Extract the text between the first and last double quotes
+    if first_quote != -1 and last_quote != -1 and first_quote != last_quote:
+        return input_string[first_quote + 1:last_quote]
+    else:
+        return input_string
+    
+
+async def chat_completion(prompt):
+   
+    url = "https://api.openai.com/v1/chat/completions"
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helping to extend prompt for creating detailed image"
+        },
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAPI_KEY}"
+    }
+    data = {
+        "model": "gpt-4o",
+        "messages": messages
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    logging.info(response.json())  # Ensure the response is parsed as JSON
+    
+    text = response.json()['choices'][0]['message']['content']
+    
+    return extract_text(text)
+    # chunk_size = 4096
+    
+    # for i in range(0, len(text), chunk_size):
+    #     await update.message.reply_text(text[i:i+chunk_size])
+        
+    # # await update.message.reply_text(response.json())
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -59,7 +153,9 @@ async def create_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     # Removing "/make" from the string
     result = update.message.text.replace("/async", "").strip()
     
-    text_identifier = await update.message.reply_text("Starting generate...")
+    result = await chat_completion(result)
+    await update.message.reply_text(result)
+    text_identifier = await update.message.reply_text("Starting generate ...")
 
     job_id = await call_fooocus_async(result, "Speed")
     job_status = await get_job_status(job_id)
@@ -141,6 +237,7 @@ def setup_handlers(app):
     app.add_handler(CommandHandler("make", make))
     app.add_handler(CommandHandler("async", make_async))
     app.add_handler(CommandHandler("help", help))
+    app.add_handler(CommandHandler("igpt", image_gpt))
     # app.add_handler(CommandHandler("audio", audio))
     app.add_handler(MessageHandler(filters.ALL, audio))
     
